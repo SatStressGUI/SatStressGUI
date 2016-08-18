a8df88e420098d2fffe2ff18f86e1e437086ec77
SatStressGUI <satstressgui@gmail.com>
2016-08-18 10:28:23 -0700

    Update satstressgui.py

--- Contents/Resources/satstressgui.py
+++ Contents/Resources/satstressgui.py
@@ -105,7 +105,7 @@ class SatelliteCalculation(object):
         ("DENSITY", u"Density [kg/m3]"),
         ("YOUNGS_MODULUS", u"Young's Modulus [Pa]"),
         ("POISSONS_RATIO", u"Poisson's Ratio"),
-        ("THICKNESS", u"Thickness_in [m]"),
+        ("THICKNESS", u"Thickness [m]"),
         ("VISCOSITY", u"Viscosity [Pa s]")]
 
     satlayers_d = [
